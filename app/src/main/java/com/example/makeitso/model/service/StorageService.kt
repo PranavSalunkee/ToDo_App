e53573a21f5dd97b4a3c4f5f09134e8b1010eab7
Pranav Salunke <pranavsalunke2021@gmail.com>
2024-01-07 01:07:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/makeitso/model/service/StorageService.kt
@@ -0,0 +1,14 @@
+
+
+package com.example.makeitso.model.service
+
+import com.example.makeitso.model.Task
+import kotlinx.coroutines.flow.Flow
+
+interface StorageService {
+  val tasks: Flow<List<Task>>
+  suspend fun getTask(taskId: String): Task?
+  suspend fun save(task: Task): String
+  suspend fun update(task: Task)
+  suspend fun delete(taskId: String)
+}
---
+++ app/src/main/java/com/example/makeitso/screens/tasks/TasksViewModel.kt
@@ -0,0 +1,54 @@
+
+package com.example.makeitso.screens.tasks
+
+import androidx.compose.runtime.mutableStateOf
+import com.example.makeitso.EDIT_TASK_SCREEN
+import com.example.makeitso.SETTINGS_SCREEN
+import com.example.makeitso.TASK_ID
+import com.example.makeitso.model.Task
+import com.example.makeitso.model.service.ConfigurationService
+import com.example.makeitso.model.service.LogService
+import com.example.makeitso.model.service.StorageService
+import com.example.makeitso.screens.MakeItSoViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.emptyFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class TasksViewModel @Inject constructor(
+  logService: LogService,
+  private val storageService: StorageService,
+  private val configurationService: ConfigurationService
+) : MakeItSoViewModel(logService) {
+  val options = mutableStateOf<List<String>>(listOf())
+
+  val tasks = storageService.tasks
+
+  fun loadTaskOptions() {
+    //TODO
+  }
+
+  fun onTaskCheckChange(task: Task) {
+    launchCatching { storageService.update(task.copy(completed = !task.completed)) }
+  }
+
+  fun onAddClick(openScreen: (String) -> Unit) = openScreen(EDIT_TASK_SCREEN)
+
+  fun onSettingsClick(openScreen: (String) -> Unit) = openScreen(SETTINGS_SCREEN)
+
+  fun onTaskActionClick(openScreen: (String) -> Unit, task: Task, action: String) {
+    when (TaskActionOption.getByTitle(action)) {
+      TaskActionOption.EditTask -> openScreen("$EDIT_TASK_SCREEN?$TASK_ID={${task.id}}")
+      TaskActionOption.ToggleFlag -> onFlagTaskClick(task)
+      TaskActionOption.DeleteTask -> onDeleteTaskClick(task)
+    }
+  }
+
+  private fun onFlagTaskClick(task: Task) {
+    launchCatching { storageService.update(task.copy(flag = !task.flag)) }
+  }
+
+  private fun onDeleteTaskClick(task: Task) {
+    launchCatching { storageService.delete(task.id) }
+  }
+}
